2b790c6de9223698825c43e527ee7960f2ff9e3b
LukeHart40 <lucas.hart@mcusd709.org>
2024-08-21 22:32:15 -0500

    Create AboutMe.java
    AboutMe creation

---
+++ AboutMe.java
@@ -0,0 +1,8 @@
+public class AboutMe{
+    public static void main(String[] args){
+        System.out.println("I am Luke Hart");
+        System.out.println("I am 18 years old");
+        System.out.println("I am in love with the band called Evanescence");
+        System.out.println("I am a computer enthusiast");
+    }
+}
