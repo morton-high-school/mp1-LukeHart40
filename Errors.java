82329294aeb1f9c750c985ccacd371043eb5271e
LukeHart40 <lucas.hart@mcusd709.org>
2024-08-21 22:15:37 -0500

    Update Errors.java
    Changes 4

--- Errors.java
+++ Errors.java
@@ -5,11 +5,11 @@ public class Error{
   public static main(string[] args){
     String name = "Alan";
     String animal = "duck";
-    System.out.println("My name is " + Name + " and I am a " + animal + ".");
+    System.out.println("My name is " + name + " and I am a " + animal + ".");
 
     int weight = 90;
     int volume = 8;
-    double density = weight/volume; // Should be the correct precise value.
+    double density = (weight/volume); // Should be the correct precise value.
 
     System.out.println("I am a heavy " + animal + " because I have a density of " + density + " kilograms per cubic centimeter.");
 
